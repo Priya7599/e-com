195aac0e6434721a9108263357eb9c631a39894e
Priya Sharma <priya751999@gmail.com>
2022-02-04 08:55:48 +0530

    initial commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// // (function(){
+
+// var ProductImg = document.getElementById("ProductImg").innerHTML;
+// var SmallImg = document.getElementByClassName("small-img").innerHTML;
+
+// SmallImg[0].onclick = function()
+// {
+//     ProductImg.src = SmallImg[0].src;
+// }
+// SmallImg[1].onclick = function()
+// {
+//     ProductImg.src = SmallImg[1].src;
+// }
+// SmallImg[2].onclick = function()
+// {
+//     ProductImg.src = SmallImg[2].src;
+// }
+// SmallImg[3].onclick = function()
+// {
+//     ProductImg.src = SmallImg[3].src;
+// }
+// })
